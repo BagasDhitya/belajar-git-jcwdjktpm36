c861a523181c0cb41ecfd249de0a87e179f474e8
Nave765 <web3evankeane@gmail.com>
2025-07-28 15:37:36 +0700

    Add: listMaterial array

--- exerciseGit1.ts
+++ exerciseGit1.ts
@@ -1,1 +1,11 @@
-console.log('exercise git 1')
+console.log("exercise git 1");
+
+const listMaterial: string[] = [
+  "add",
+  "commit",
+  "push",
+  "pull",
+  "rebase",
+  "conflict",
+  "branch",
+];
